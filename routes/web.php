800814afa3cb6b68be4adcc870e252692bca7d36
dante <dantinr@gmail.com>
2020-03-13 15:02:04 +0800

    支付模块

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 
 Route::get('/github','GithubController@index'); ##GITHUB登录
 Route::get('/github/callback2','GithubController@callback2'); ##用户授权回跳的页面
+Route::get('/github/aaa','GithubController@callback2'); ##用户授权回跳的页面
 
 
 
